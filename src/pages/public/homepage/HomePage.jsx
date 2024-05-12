218bce54291281f36718b0315f0a39d3ea095c6d
Dimas712 <jasinga1933@gmail.com>
2024-05-12 17:46:39 +0700

    commit

---
+++ src/pages/public/homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import AboutSection from "./sections/about.section";
+import HeroSection from "./sections/hero.section";
+import StepSection from "./sections/step.section";
+
+function HomePage() {
+  return (
+    <div className="flex flex-col container max-w-screen-xl overflow-hidden md:overflow-visible">
+      <HeroSection />
+      <StepSection />
+      <AboutSection />
+    </div>
+  );
+}
+
+export default HomePage
